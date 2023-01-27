2106d891e9a85054ed6729f17611343a99c590b6
Evan <j.evan.woods@gmail.com>
2023-01-27 10:57:21 -0500

    Added operate function with console.log only!
    This calls previous math functions, but no returns.

--- script.js
+++ script.js
@@ -13,3 +13,21 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 };
+
+function operate(operator, numberOne, numberTwo) {
+    if (operator === "plus") {
+        let sum = add(numberOne,numberTwo);
+        console.log(sum);
+    } else if (operator === "minus") {
+        let difference = subtract(numberOne, numberTwo);
+        console.log(difference);
+    } else if (operator === "times") {
+        let multiplication = multiply(numberOne, numberTwo);
+        console.log(multiplication);
+    } else if (operator === "divided") {
+        let division = divide(numberOne, numberTwo);
+        console.log(division);
+    } else {
+        console.log("ERROR")
+    }
+};
