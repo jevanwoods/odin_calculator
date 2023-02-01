7c61d990c6826162bbf2baa4e0a95b300133a887
Evan <j.evan.woods@gmail.com>
2023-02-01 07:52:39 -0500

    Corrected number functions and corresponding divs.

--- script.js
+++ script.js
@@ -36,42 +36,85 @@ const lastEntered = document.querySelector('#lastEntered');
 const currentEntered = document.querySelector('currentEntered');
 
 function getZero() {
-    document.getElementById('currentEntered').innerHTML = "0";
+    let curNum = document.getElementById('currentEntered').innerHTML;
+    newNum = curNum + 0;
+    actNum = +newNum;  
+    document.getElementById('currentEntered').innerHTML = actNum;
 };
 
 function getOne() {
-    document.getElementById('currentEntered').innerHTML = "1";
+    let curNum = document.getElementById('currentEntered').innerHTML;
+    newNum = curNum + 1;
+    actNum = +newNum;  
+    document.getElementById('currentEntered').innerHTML = actNum;
 };
 
 function getTwo() {
-    document.getElementById('currentEntered').innerHTML = "2";
+    let curNum = document.getElementById('currentEntered').innerHTML;
+    newNum = curNum + 2;
+    actNum = +newNum;  
+    document.getElementById('currentEntered').innerHTML = actNum;
 };
 
 function getThree() {
-    document.getElementById('currentEntered').innerHTML = "3";
+    let curNum = document.getElementById('currentEntered').innerHTML;
+    newNum = curNum + 3;
+    actNum = +newNum;  
+    document.getElementById('currentEntered').innerHTML = actNum;
 };
 
 function getFour() {
-    document.getElementById('currentEntered').innerHTML = "4";
+    let curNum = document.getElementById('currentEntered').innerHTML;
+    newNum = curNum + 4;
+    actNum = +newNum;  
+    document.getElementById('currentEntered').innerHTML = actNum;
 };
 
 function getFive() {
-    document.getElementById('currentEntered').innerHTML = "5";
+    let curNum = document.getElementById('currentEntered').innerHTML;
+    newNum = curNum + 5;
+    actNum = +newNum;  
+    document.getElementById('currentEntered').innerHTML = actNum;
 };
 
 function getSix() {
-    document.getElementById('currentEntered').innerHTML = "6";
+    let curNum = document.getElementById('currentEntered').innerHTML;
+    newNum = curNum + 6;
+    actNum = +newNum;  
+    document.getElementById('currentEntered').innerHTML = actNum;
 };
 
 function getSeven() {
-    document.getElementById('currentEntered').innerHTML = "7";
+    let curNum = document.getElementById('currentEntered').innerHTML;
+    newNum = curNum + 7;
+    actNum = +newNum;  
+    document.getElementById('currentEntered').innerHTML = actNum;
 };
 
 function getEight() {
-    document.getElementById('currentEntered').innerHTML = "8";
+    let curNum = document.getElementById('currentEntered').innerHTML;
+    newNum = curNum + 8;
+    actNum = +newNum;  
+    document.getElementById('currentEntered').innerHTML = actNum;
 };
 
 function getNine() {
-    document.getElementById('currentEntered').innerHTML = "9";
+    let curNum = document.getElementById('currentEntered').innerHTML;
+    newNum = curNum + 9;
+    actNum = +newNum;  
+    document.getElementById('currentEntered').innerHTML = actNum;
 };
 
+function clear() {
+    let curNum = document.getElementById('currentEntered').innerHTML;
+    curNum = '';
+    actNum = +curNum;
+    document.getElementById('currentEntered').innerHTML = actNum;
+}
+
+function plus() {
+    let curNum = document.getElementById('currentEntered').innerHTML;
+    let lastNum = document.getElementById('lastEntered').innerHTML;
+    lastNum = +curNum;
+    document.getElementById('lastEntered').innerHTML = lastNum + '+';
+}
